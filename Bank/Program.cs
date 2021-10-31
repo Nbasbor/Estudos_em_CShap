801f3529fe1896ebd109745bb049d9b8e4805a5d
Basbor <nanbasbor@gmail.com>
2021-10-31 19:14:21 -0300

    Adicionando funcionalidade Funcionários - Estudos de Herança

---
+++ Bank/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bank.Funcionario;
+
+namespace Bank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            GerenciadorBonificacao gerenciador = new GerenciadorBonificacao();
+            Funcionario funcionario = new Funcionario();
+
+            funcionario.Nome = "Carlos";
+            funcionario.CPF = "12346578910";
+            funcionario.Salario = 1500.00
+        }
+    }
+}
