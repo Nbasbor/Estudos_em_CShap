391288703bfb679786aefa9ecaa1cf52f6595169
Ananda Bastida Borges <nanbasbor@gmail.com>
2022-01-07 17:07:17 -0300

    adicionando classe usuario e conexão com Banco de Dados

---
+++ CurriculoCadastroBD/CurriculoCadastroBD/Curriculo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace CurriculoCadastroBD
+{
+    public class Curriculo
+    {
+        string strCon;
+        SqlConnection con;
+
+        public Curriculo()
+        {
+            strCon = "Data Source=WINAP0KZ3IOST0R\\SQLEXPRESS;  " + //servidor sqlserver \\ para especificar que tem uma barra invertida e não é \n
+                            "Initial Catalog=curriculo; " + //banco que será conectado
+                            "Integrated Security=True";//ativando a segurança integrada do windows
+        }
+        public SqlConnection GetConnection()
+        {
+            con = new SqlConnection(strCon);
+            try
+            {
+                con.Open();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Falha ao conectar o banco\n" + ex.Message);
+            }
+            return con;
+        }
+    }
+}
---
+++ CurriculoCadastroBD/CurriculoCadastroBD/Usuario.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CurriculoCadastroBD
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string Senha { get; set; }
+        public string CPF { get; set; }
+        public string Profissao { get; set; }
+        public string Area { get; set; }
+        public string Nacionalidade { get; set; }
+        public string Estado { get; set; }
+        public string Cidade { get; set; }
+        public string Endereco { get; set; }
+        
+        public Usuario()
+        {
+
+        }
+
+        public Usuario(int id, string nome, string email, string senha, string cpf, string profissao, string area, string nacionalidade, string estado, string cidade, string endereco)
+        {
+            Id = id;
+            Nome = nome;
+            Email = email;
+            Senha = senha;
+            CPF = cpf;
+            Profissao = profissao;
+            Area = area;
+            Nacionalidade = nacionalidade;
+            Estado = estado;
+            Cidade = cidade;
+            Endereco = endereco;
+        }
+    }
+}
