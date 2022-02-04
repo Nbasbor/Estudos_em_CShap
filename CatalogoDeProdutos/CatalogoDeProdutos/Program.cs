62ef43c4a8a485887e15a702dacda31a828437cd
Ananda Bastida Borges <nanbasbor@gmail.com>
2022-02-04 16:31:35 -0300

    ASP.NET MVC

--- CatalogoDeProdutos/CatalogoDeProdutos/Program.cs
+++ CatalogoDeProdutos/CatalogoDeProdutos/Program.cs
@@ -9,7 +9,7 @@ builder.Services.AddControllersWithViews();
 //IServiceProvider serviceProvider = builder.Services; 
 var app = builder.Build();
 
-IServiceProvider serviceProvider = app.Services; //Adiciona o IServiceProvider
+//IServiceProvider serviceProvider = app.Services; //Adiciona o IServiceProvider
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
@@ -17,14 +17,14 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-void ConfigureServices(IServiceCollection services)
-{
-    // services.AddTransient<ICatalogo, Catalogo>();//Cria um serviço transitorios
-    // services.AddTransient<IRelatorios, Relatorios>();   
-    services.AddScoped<ICatalogo, Catalogo>();//Cria um serviço com escopo
-    services.AddScoped<IRelatorios, Relatorios>();
+//void ConfigureServices(IServiceCollection services)
+//{
+//    // services.AddTransient<ICatalogo, Catalogo>();//Cria um serviço transitorios
+//    // services.AddTransient<IRelatorios, Relatorios>();   
+//    services.AddScoped<ICatalogo, Catalogo>();//Cria um serviço com escopo
+//    services.AddScoped<IRelatorios, Relatorios>();
     
-}
+//}
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
@@ -36,12 +36,14 @@ app.UseAuthorization();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+app.Run();
 
-ICatalogo catalogo = serviceProvider.GetService<ICatalogo>();     //Injeção de dependencia
-IRelatorios relatorio = serviceProvider.GetService<IRelatorios>();//o catalogo como parametro
 
-app.Run(async (context) =>
-{
-   await relatorio.Imprimir(context); // método await espera a execução do método
+//ICatalogo catalogo = serviceProvider.GetService<ICatalogo>();     //Injeção de dependencia
+//IRelatorios relatorio = serviceProvider.GetService<IRelatorios>();//o catalogo como parametro
 
-});
+//app.Run(async (context) =>
+//{
+//   await relatorio.Imprimir(context); // método await espera a execução do método
+
+//});
