b761c29d9494bd1a4b4372a47a96c2de4a5d31fb
Ananda Bastida Borges <nanbasbor@gmail.com>
2022-02-09 15:12:03 -0300

    Adicionando contexto de Banco de dados ao projeto

---
+++ CadastroPerfil/cadastroPerfil/Operacao.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cadastroPerfil
+{
+
+    public static class Acao
+    {
+        public enum Operacao { edit, del, add };
+    }
+}
