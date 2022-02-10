2f1971643ec940d153150e99b76f9cc07b64778a
Ananda Bastida Borges <nanbasbor@gmail.com>
2022-02-10 16:45:27 -0300

    implementando RequestDelegate

--- WebApp/WebApp/Startup.cs
+++ WebApp/WebApp/Startup.cs
@@ -15,16 +15,18 @@ namespace WebApp
 
             //Dicionario dos caminhos
             var _repo = new EstudoRepositorioCVS();
-            var caminhosAtendidos = new Dictionary<string, string> //configura os caminhos da request
+            var caminhosAtendidos = new Dictionary<string, RequestDelegate> //configura os caminhos da request como string e requesteDelegate
             {
-                { "/Estudos/ParaEstudar", _repo.ParaEstudar.ToString()},
-                { "/Estudos/Estudando", _repo.Estudando.ToString()}
+                { "/Estudos/ParaEstudar", EstudosParaEstudar },
+                { "/Estudos/Estudando", EstudosEstudando }
             };
 
             if (caminhosAtendidos.ContainsKey(contexto.Request.Path))
             {
-                return contexto.Response.WriteAsync(caminhosAtendidos[contexto.Request.Path]);//da como resposta o valor que esta nos caminhos
+                var metodo = caminhosAtendidos[contexto.Request.Path];//da como resposta o valor que esta nos caminhos
+                return metodo.Invoke(contexto);//retorna uma variavel request delegate
             }
+            contexto.Response.StatusCode = 404;
             return contexto.Response.WriteAsync("Caminho inexistente");//Metódo de caminho da requisição
         }
         public Task EstudosParaEstudar(HttpContext contexto)    
@@ -35,6 +37,11 @@ namespace WebApp
 
             return contexto.Response.WriteAsync(_repo.ParaEstudar.ToString());//Escreve como resposta asyncrona a lista Para estudar
            
+        }
+        public Task EstudosEstudando(HttpContext contexto)
+        {
+            var _repo = new EstudoRepositorioCVS();
+            return contexto.Response.WriteAsync(_repo.Estudando.ToString());
         }
     }
 }
